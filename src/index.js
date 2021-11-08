8dd1b6a233ee7d1725654478d03c784a7be36fe3
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-11-08 02:08:31 -0800

    Finished my function which will convert the timestamp that the weather data returns and it will return an actual date that I will use when I display the data to users

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import { getWeatherTodayByLocation } from './weather-logic/weather-functions';
+import {
+  getWeatherTodayByLocation,
+  convertTimestampToDate,
+} from './weather-logic/weather-functions';
 
 getWeatherTodayByLocation('Vancouver', 'metric')
   .then((response) => {
+    console.log(response);
+    return response;
+  })
+  .then((response) => {
+    response.current.dt = convertTimestampToDate(response.current.dt);
+    console.log(response.current.dt);
     console.log(response);
   })
   .catch((err) => console.log(err));
--- src/weather-logic/weather-functions.js
+++ src/weather-logic/weather-functions.js
@@ -11,11 +11,33 @@ async function getLatitudeAndLongtitude(city) {
 async function getWeatherTodayByLocation(city, unit) {
   const [latitude, longitude] = await getLatitudeAndLongtitude(city);
   const weatherResponse = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,hourly,minutely,alerts&appid=712b9aa6ed9063798a216524da6bb71e&units=${unit}`,
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts&appid=712b9aa6ed9063798a216524da6bb71e&units=${unit}`,
     // eslint-disable-next-line comma-dangle
     { mode: 'cors' }
   );
   const weatherData = await weatherResponse.json();
   return weatherData;
 }
-export { getWeatherTodayByLocation, getLatitudeAndLongtitude };
+function convertTimestampToDate(timeStamp) {
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const currentDate = new Date(timeStamp * 1000);
+  return `${monthNames[currentDate.getMonth()]} ${currentDate.getDate()}`;
+}
+export {
+  getWeatherTodayByLocation,
+  getLatitudeAndLongtitude,
+  convertTimestampToDate,
+};
