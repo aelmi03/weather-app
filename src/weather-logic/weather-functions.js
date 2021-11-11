1de5df0585befc65088bba51499cd968a3752b99
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-11-10 21:00:37 -0800

    Finished the function which takes the weather response object and converts it to an object with only the properties i need

--- src/weather-logic/weather-functions.js
+++ src/weather-logic/weather-functions.js
@@ -1,7 +1,9 @@
+/* eslint-disable comma-dangle */
+import { fromUnixTime } from 'date-fns';
+
 async function getLatitudeAndLongtitude(city) {
   const weatherResponse = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=712b9aa6ed9063798a216524da6bb71e`,
-    // eslint-disable-next-line comma-dangle
     { mode: 'cors' }
   );
   const weatherData = await weatherResponse.json();
@@ -12,32 +14,47 @@ async function getWeatherTodayByLocation(city, unit) {
   const [latitude, longitude] = await getLatitudeAndLongtitude(city);
   const weatherResponse = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts&appid=712b9aa6ed9063798a216524da6bb71e&units=${unit}`,
-    // eslint-disable-next-line comma-dangle
     { mode: 'cors' }
   );
   const weatherData = await weatherResponse.json();
   return weatherData;
 }
-function convertTimestampToDate(timeStamp) {
-  const monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  const currentDate = new Date(timeStamp * 1000);
-  return `${monthNames[currentDate.getMonth()]} ${currentDate.getDate()}`;
+function convertUnixTimestampToHours(timeStamp) {
+  const date = fromUnixTime(timeStamp);
+  // eslint-disable-next-line prettier/prettier
+  const minutes = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`;
+  return `${date.getHours()}:${minutes}}`;
 }
+function transformToCurrentWeatherObject(weatherObject, weatherCity) {
+  const city = weatherCity;
+  const sunrise = convertUnixTimestampToHours(weatherObject.current.sunrise);
+  const sunset = convertUnixTimestampToHours(weatherObject.current.sunset);
+  const {
+    // eslint-disable-next-line camelcase
+    current: { feels_like },
+  } = weatherObject;
+  const {
+    current: { temp, humidity, pressure },
+  } = weatherObject;
+  // eslint-disable-next-line prefer-destructuring
+  const { main, icon, description } = weatherObject.current.weather[0];
+
+  return {
+    sunrise,
+    sunset,
+    feels_like,
+    humidity,
+    pressure,
+    city,
+    temp,
+    main,
+    description,
+    icon,
+  };
+}
+
 export {
   getWeatherTodayByLocation,
   getLatitudeAndLongtitude,
-  convertTimestampToDate,
+  transformToCurrentWeatherObject,
 };
